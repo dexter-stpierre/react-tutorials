3ddf4b4d663d7aa6cc86f25ab3db140692aa70e0
Dexter St-Pierre <dexter.stpierre.dev@gmail.com>
2019-07-26 09:37:23 -0500

    learn toggle and lifecycle methods

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -2,26 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Welcome text="Welcome to using props" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
+  componentWillMount() {
+    console.log('will mount');
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  state = {
+    toggle: true
+  };
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Welcome text="Welcome to using props" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {this.state.toggle && <p>This should show and hide</p>}
+          <button onClick={this.toggle}>Show/Hide</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 class Welcome extends Component {
