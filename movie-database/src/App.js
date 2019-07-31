7371189bc78fe523058f973f19be8be125c55375
Dexter St Pierre <dexter.stpierre.dev@gmail.com>
2019-07-31 18:29:19 -0500

    Finished video 15
    - Added react Router to application

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -26,16 +27,21 @@ class App extends Component {
   render() {
     const { movies } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        {movies.map(movie => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Route path="/test" component={Test} />
+          {movies.map(movie => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+const Test = () => <h1>TEST</h1>;
