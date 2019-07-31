e68d77eccced0bc3c057e2ce0a101245758f1a81
Dexter St Pierre <dexter.stpierre.dev@gmail.com>
2019-07-31 16:05:15 -0500

    Completed video 9
    - Added list of movies
    - Added Movie component
    - Mow rendering a list of movies

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -2,13 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars',
+  },
+  {
+    id: 2,
+    title: 'Spiderman',
+  },
+  {
+    id: 3,
+    title: '36th Chamber of Shaolin',
+  },
+  {
+    id: 4,
+    title: "Avenger's Endgame",
+  },
+];
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
   }
