8a9db04d659d6a62a8827d29115b9c995adafe73
Dexter St Pierre <dexter.stpierre.dev@gmail.com>
2019-08-01 08:20:28 -0500

    Finished video 19
    - Added styled components
    - Added overdrive to handle transitions

--- movie-database/src/MovieDetail.js
+++ movie-database/src/MovieDetail.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import { RouteProps } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 
+import { Poster } from './Movie';
+
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
 class MoviesDetail extends Component {
-  static propTypes = RouteProps;
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.string,
+      }),
+    }).isRequired,
+  };
 
   state = {
     movie: {},
@@ -30,14 +41,41 @@ class MoviesDetail extends Component {
   render() {
     const { movie } = this.state;
     return (
-      <div>
-        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-        <h1>{movie.title}</h1>
-        <h3>{movie.release_date}</h3>
-        <p>{movie.overview}</p>
-      </div>
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+          </Overdrive>
+          <div>
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
     );
   }
 }
 
 export default MoviesDetail;
+
+const MovieWrapper = styled.div`
+  position: relative;
+  padding-top: 50vh;
+  background: url(${props => props.backdrop}) no-repeat;
+  background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+  background: white;
+  text-align: left;
+  padding: 2rem 10%;
+  display: flex;
+  > div {
+    margin-left: 20px;
+  }
+  img {
+    position: relative;
+    top: -5rem;
+  }
+`;
